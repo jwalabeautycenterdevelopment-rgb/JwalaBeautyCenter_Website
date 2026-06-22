03930f1ba09ea13941a4f1a5b56b9f862f9d80b3
Prakash-WebDad <prakash.s@webdads2u.com>
2026-06-22 11:22:05 +0530

    pagination added

--- app/components/Container/ProductDetails/RelatedProduct.jsx
+++ app/components/Container/ProductDetails/RelatedProduct.jsx
@@ -1,22 +1,24 @@
-import MainLayout from '@/app/common/MainLayout'
-import { ProductCard } from '@/app/common/ProducrCart'
-import React from 'react'
+import MainLayout from "@/app/common/MainLayout";
+import { ProductCard } from "@/app/common/ProducrCart";
+import React from "react";
 
 const RelatedProduct = ({ product }) => {
     return (
         <MainLayout>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
-                {
-                    product?.map((item) =>
-                        <div key={item?._id}>
+            <div className="overflow-x-auto scrollbar-hide">
+                <div className="flex gap-6 min-w-max pb-2">
+                    {product?.map((item) => (
+                        <div
+                            key={item?._id}
+                            className="w-[280px] flex-shrink-0"
+                        >
                             <ProductCard product={item} isHover={false} />
                         </div>
-                    )
-                }
+                    ))}
+                </div>
             </div>
-
         </MainLayout>
-    )
-}
+    );
+};
 
-export default RelatedProduct
+export default RelatedProduct;
